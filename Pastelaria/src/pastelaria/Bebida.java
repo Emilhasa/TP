cc2929170a5bcc1a4087b03d9c8e9bf0837c4c0e
Emílio <emiliofmartinss@gmail.com>
2021-09-10 18:15:31 -0300

    Codigo inicial relacionado ao TP3

---
+++ Pastelaria/src/pastelaria/Bebida.java
@@ -0,0 +1,49 @@
+package pastelaria;
+
+public class Bebida extends Produto{
+	private int volumeBebida;
+	
+	
+	public Bebida(String name, int volumeBebida, double valor) {
+		this.nome = name;
+		this.volumeBebida = volumeBebida;
+		this.valor = valor;
+	}
+	
+	public void editarBebida(String nome, int volumeBebida, double valor) {
+		setNome(nome);
+		setVolumeBebida(volumeBebida);
+		setValor(valor);		
+	}
+
+	public String toString() {
+		return "*****Bebida*****\nnome: " + nome + "\nvolumeBebida: " + volumeBebida + " ml\nvalor: R$" + valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public int getVolumeBebida() {
+		return volumeBebida;
+	}
+
+	public void setVolumeBebida(int volumeBebida) {
+		this.volumeBebida = volumeBebida;
+	}
+	
+	
+
+}
---
+++ Pastelaria/src/pastelaria/Pedido.java
@@ -0,0 +1,64 @@
+package pastelaria;
+
+import java.util.*;
+
+public abstract class Pedido {
+	protected Date data;
+	protected int hora;
+	protected int numeroPedido;
+	protected String status;
+	protected String observacoes;
+
+
+	public Date getData() {
+		return data;
+	}
+	
+	
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+	public int getHora() {
+		return hora;
+	}
+	
+	
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+	
+	
+	public int getNumeroPedido() {
+		return numeroPedido;
+	}
+	
+	
+	public void setNumeroPedido(int numeroPedido) {
+		this.numeroPedido = numeroPedido;
+	}
+	
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	public String getObservacoes() {
+		return observacoes;
+	}
+	
+	
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+	
+	
+	
+}
